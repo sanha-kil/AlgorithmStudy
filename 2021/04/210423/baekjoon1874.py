ec6fcc87e0ea05807d5f8eb018c5d7d5031d69c1
sanha <kgpaper@naver.com>
2021-04-26 17:03:30 +0900

    21/04/26 boj 1874 solve

--- 2021/04/210423/baekjoon1874.py
+++ 2021/04/210423/baekjoon1874.py
@@ -7,3 +7,22 @@ s = []
 for i in range(n):
   s.append(int(input()))
 
+stack = []
+answer = []
+count = 0
+
+for i in range(1, n+1):
+  stack.append(i)
+  answer.append('+')
+  while stack[-1] == s[count]:
+    stack.pop()
+    answer.append('-')
+    count += 1
+    if not stack:
+      break
+
+if not stack:
+  for i in range(len(answer)):
+    print(answer[i])
+else:
+  print('NO')
