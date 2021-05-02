4de703c00f5a86ea3382e2bb20c3bc3e0877c7f0
sanha <kgpaper@naver.com>
2021-05-02 19:56:15 +0900

    21/05/02 boj 5430 solve

--- 2021/05/baekjoon5430.py
+++ 2021/05/baekjoon5430.py
@@ -10,15 +10,28 @@ for _ in range(int(input())):
     que = deque([])
   else:
     que = deque(arr)
-  command = command.replace('rr','')
-  if que != 'error':
-    for a in command:
-      try:
-        if a == 'r':
-          que.reverse()
-        if a == 'd':
-          que.popleft()
-      except:
-        que = 'error'
-        break
+  command = command.replace('RR','')
   
+  check = True
+  isReversed = False
+  
+  for a in command:
+    try:
+      if a == 'R':
+        isReversed = not isReversed
+      if a == 'D':
+        if isReversed == False:
+          que.popleft()
+        else:
+          que.pop()
+    except:
+      check = False
+      break
+
+  if isReversed == True:
+    que.reverse()
+
+  if check == False:
+    print('error')
+  else:
+    print('['+ ','.join(que) +']')
