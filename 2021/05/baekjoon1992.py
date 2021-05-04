75e020115601f304b47e41f875c4c1940a3004df
sanha <kgpaper@naver.com>
2021-05-04 18:49:57 +0900

    21/05/04 boj 1992 solve

--- 2021/05/baekjoon1992.py
+++ 2021/05/baekjoon1992.py
@@ -11,21 +11,21 @@ def quad_tree(p, n):
 
     for j in range(n):
       if p[i][j] != check:
+        print('(', end='')
         quad_tree([row[:n//2] for row in p[:n//2]], n//2)
         quad_tree([row[n//2:n] for row in p[:n//2]], n//2)
         quad_tree([row[:n//2] for row in p[n//2:n]], n//2)
         quad_tree([row[n//2:n] for row in p[n//2:n]], n//2)
         ex = True
+        print(')', end='')
         break
     
   if not ex:
-    if check == 1:
-      blue += 1
-    else:
-      white += 1
+    print(check, end='')
 
 n = int(input())
 paper = []
 for i in range(n):
-  paper.append(input().strip().split())
-  print(paper[i])
+  paper.append(input().strip())
+
+quad_tree(paper, n)
