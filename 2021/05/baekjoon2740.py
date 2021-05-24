2c3747c60616005dfcb93944503b1dabbc9ec33e
sanha <kgpaper@naver.com>
2021-05-24 16:23:46 +0900

    21/05/24 boj 2740 solve

--- 2021/05/baekjoon2740.py
+++ 2021/05/baekjoon2740.py
@@ -1,5 +1,27 @@
 import sys
 input = sys.stdin.readline
 
-for _ in range(2):
-  input 
+n, m = map(int, input().split())
+A = []
+for _ in range(n):
+    A.append(list(map(int, input().split())))
+
+m, k = map(int, input().split())
+B = []
+for _ in range(m):
+    B.append(list(map(int, input().split())))
+
+
+#행렬 곱셈
+C = [[0 for _ in range(k)] for _ in range(n)]
+
+for i in range(n):
+    for j in range(k):
+        for l in range(m):
+            C[i][j] += A[i][l] * B[l][j]
+
+#출력문
+for i in C:
+    for j in i:
+        print(j, end = ' ')
+    print('')
