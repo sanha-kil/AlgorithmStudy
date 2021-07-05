52dcab1c3704204b60f356469b98b7b3c2c3fabe
sanha <kgpaper@naver.com>
2021-07-06 01:50:46 +0900

    21/07/06 pg solve

--- 2021/07/pg_구명보트.py
+++ 2021/07/pg_구명보트.py
@@ -1,17 +1,14 @@
 # 프로그래머스 2단계 "구명보트"
 # 코딩테스트 고득점 Kit 포함 문제
 
-import heapq
-
 def solution(people, limit):
   answer = 0
-  people.sort(reverse = True)
-  while people:
-    k = people.pop()
-    for i in range(len(people)):
-      if k + people[i] <= limit:
-        people.pop(i)
-        break
+  people.sort()
+  i, j = 0, len(people)-1
+  while i <= j:
+    if people[i] + people[j] <= limit:
+      i += 1
+    j-=1
     answer += 1
 
   return answer
