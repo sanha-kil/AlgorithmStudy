015aa899311d16af01079bcef46cff3d767f52eb
sanha <kgpaper@naver.com>
2021-07-28 19:20:00 +0900

    21/07/28 studying..

---
+++ 2021/07/boj_11066.py
@@ -0,0 +1,10 @@
+# 백준 11066번 '파일 합치기'
+# DP
+
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+  k = int(input())
+  
---
+++ 2021/07/boj_1260.py
@@ -0,0 +1,12 @@
+# 백준 1260번 'DFS와 BFS'
+# DFS, BFS
+
+import sys
+input = sys.stdin.readline
+
+n, m, v = map(int, input().split())
+line = {}
+for _ in range(m):
+  a, b = map(int, input().split())
+  
+
